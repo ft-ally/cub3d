b918b0360e805d186ea9d805c092955f8b9ef3fe
Tutku Cakir Yilmaz <tcakir-y@f1r1s7.codam.nl>
2025-10-22 16:49:51 +0200

    FIX norminette errors, added todo free for later

--- src/init_game.c
+++ src/init_game.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_game.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tutku <tutku@student.42.fr>                +#+  +:+       +#+        */
+/*   By: tcakir-y <tcakir-y@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 17:15:25 by tcakir-y          #+#    #+#             */
-/*   Updated: 2025/10/21 20:00:20 by tutku            ###   ########.fr       */
+/*   Updated: 2025/10/22 16:49:26 by tcakir-y         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	pressed_esc(t_game *game)
 {
-	//free everything
+	//TODO: free everything
 	ft_free_mlx(game, ESC_PRESS);
 	exit(0);
 }
@@ -23,7 +23,7 @@ int key_hook(int keycode, t_game *game)
 {
 	if (keycode == KEY_ESC)
 	{
-		ft_free_mlx(game, ESC_PRESS); //add free for parsing
+		ft_free_mlx(game, ESC_PRESS); //TODO: add free for parsing
 		exit(0);
 	}
 	return (SUCCESS);
@@ -39,7 +39,7 @@ int	render_frame(void *param)
 	color_ceiling(game);
 	color_floor(game);
 	if (init_dda(game) != SUCCESS);
-		return (ERROR); //free shit 
+		return (ERROR); //TODO: add free
 	mlx_put_image_to_window(game->gfx.mlx, game->gfx.win,
 		game->gfx.image.img, 0, 0);
 	return (SUCCESS);
@@ -60,7 +60,7 @@ int	init_game(t_game *game)
 	if (init_mlx(game) != SUCCESS)
 		return (ERROR);
 	if (init_hooks(game) != SUCCESS)
-		return (ft_free_mlx(game, ERROR));
+		return (ft_free_mlx(game, ERROR)); //TODO: add free
 	mlx_loop(game->gfx.mlx);
 	return (SUCCESS);
 }
--- src/init_test.c
+++ src/init_test.c
@@ -3,75 +3,75 @@
 /*                                                        :::      ::::::::   */
 /*   init_test.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tutku <tutku@student.42.fr>                +#+  +:+       +#+        */
+/*   By: tcakir-y <tcakir-y@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 16:53:17 by tutku             #+#    #+#             */
-/*   Updated: 2025/10/20 19:24:38 by tutku            ###   ########.fr       */
+/*   Updated: 2025/10/22 16:48:26 by tcakir-y         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
 static const char *MAP_LIT[] = {
-    "11111",
-    "1N001",
-    "10001",
-    "10001",
-    "11111",
-    NULL
+	"11111",
+	"1N001",
+	"10001",
+	"10001",
+	"11111",
+	NULL
 };
 
-int init_test(t_game *game)
+int	init_test(t_game *game)
 {
-    game->ceiling_rgb = (8 << 16) | (148 << 8) | 159;
-    game->floor_rgb   = (9 << 16) | (54 << 8) | 108;
-    game->textures[NORTH] = "./textures/north.xpm";
-    game->textures[SOUTH] = "./textures/south.xpm";
-    game->textures[WEST] = "./textures/west.xpm";
-    game->textures[EAST] = "./textures/east.xpm";
-    return (SUCCESS);
+	game->ceiling_rgb = (8 << 16) | (148 << 8) | 159;
+	game->floor_rgb   = (9 << 16) | (54 << 8) | 108;
+	game->textures[NORTH] = "./textures/north.xpm";
+	game->textures[SOUTH] = "./textures/south.xpm";
+	game->textures[WEST] = "./textures/west.xpm";
+	game->textures[EAST] = "./textures/east.xpm";
+	return (SUCCESS);
 }
 
-int setup_map_writable(t_game *game)
+int	setup_map_writable(t_game *game)
 {
-    int h = 0;
-    while (MAP_LIT[h])
-        h++;
+	int	h = 0;
 
-    game->map = malloc(sizeof(t_map));
-    if (!game->map)
-        return print_error("malloc map failed");
+	while (MAP_LIT[h])
+		h++;
 
-    game->map->map_grid = malloc((h + 1) * sizeof(char *));
-    if (!game->map->map_grid)
-        return print_error("malloc map_grid failed");
+	game->map = malloc(sizeof(t_map));
+	if (!game->map)
+		return print_error("malloc map failed");
 
-    for (int i = 0; i < h; i++) {
-        game->map->map_grid[i] = ft_strdup(MAP_LIT[i]);
-        if (!game->map->map_grid[i]) {
-            // rollback on failure
-            while (--i >= 0) free(game->map->map_grid[i]);
-            free(game->map->map_grid);
-            free(game->map);
-            game->map = NULL;
-            return print_error("strdup failed");
-        }
-    }
-    game->map->map_grid[h] = NULL;
-    return SUCCESS;
-}
+	game->map->map_grid = malloc((h + 1) * sizeof(char *));
+	if (!game->map->map_grid)
+		return print_error("malloc map_grid failed");
 
-void free_map(t_map *m)
+	for (int i = 0; i < h; i++) {
+		game->map->map_grid[i] = ft_strdup(MAP_LIT[i]);
+		if (!game->map->map_grid[i]) {
+			while (--i >= 0) free(game->map->map_grid[i]);
+			free(game->map->map_grid);
+			free(game->map);
+			game->map = NULL;
+			return (print_error("strdup failed"));
+		}
+	}
+	game->map->map_grid[h] = NULL;
+	return SUCCESS;
+	}
+
+void	free_map(t_map *m)
 {
-    if (m)
-    {
-        if (m->map_grid)
-        {
-            for (int i = 0; m->map_grid[i]; i++)
-                free(m->map_grid[i]);
-            free(m->map_grid);
-        }
-        free(m);
-        m = NULL;
-    }
+	if (m)
+	{
+		if (m->map_grid)
+		{
+			for (int i = 0; m->map_grid[i]; i++)
+				free(m->map_grid[i]);
+			free(m->map_grid);
+		}
+		free(m);
+		m = NULL;
+	}
 }
